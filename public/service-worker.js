7a18a3682c36247ad42b24bb506a5820808e5e49
Jada Desormeaux <JadaE789@gmail.com>
2021-07-06 23:31:54 -0400

    manifest fixes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,15 @@
 const FILES_TO_CACHE = [
-    "/public/", 
-    "/public/db.js",
-    "/public/index.html", 
-    "/public/index.js",  
-    "/public/styles.css", 
-    "/public/icons/icon-192x192.png", 
-    "/public/icons/icon-512x512.png"
-  ];
-  
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1";
+  "/public/", 
+  "/public/db.js",
+  "/public/index.html", 
+  "/public/index.js",  
+  "/public/styles.css", 
+  "/public/icons/icon-192x192.png", 
+  "/public/icons/icon-512x512.png"
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
   
   // install
   self.addEventListener("install", function (evt) {
@@ -73,4 +73,3 @@ const FILES_TO_CACHE = [
       })
     );
 });
-  
